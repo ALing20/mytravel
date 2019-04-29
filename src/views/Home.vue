2b0a1483ad3ebe5f5f0ac6e4010894fea25bf2c8
ALing <1214845342@qq.com>
2019-04-29 21:03:32 +0800

    icons-end

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,21 +2,26 @@
   <div>
     <home-header></home-header>
     <home-swiper></home-swiper>
+    <home-icons></home-icons>
   </div>
 </template>
 
 <script>
 import HomeHeader from '../components/Header.vue'
 import HomeSwiper from '../components/swiper.vue'
+import HomeIcons from '../components/Icons.vue'
 export default {
   name: 'Home',
   components: {
     HomeHeader,
-    HomeSwiper
+    HomeSwiper,
+    HomeIcons
   }
 }
 </script>
 
-<style lang="">
-  
+<style lang="less">
+  body {
+    background-color: #f5f5f5;
+  }
 </style>
